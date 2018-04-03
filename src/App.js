b5cc2c097d26e0cbf2868eee04f1ffadd4925939
carcruz <cruz5114@gmail.com>
2018-04-02 18:37:20 -0600

    create private routes and Routers

--- src/App.js
+++ src/App.js
@@ -1,34 +1,44 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import { Navbar, Nav, NavItem, MenuItem, NavDropdown } from 'react-bootstrap';
-import './App.css';
+import React, { Component } from "react";
+import { BrowserRouter as Router, Route, Switch, Redirect } from "react-router-dom";
+import Nabvar from "./Navbar";
+import Login from "./Login";
+import fakeAuth from './auth';
+import "./App.css";
+
+const PrivateRoute = ({ component: Component, ...rest }) => (
+  <Route
+    {...rest}
+    render={props =>
+      fakeAuth.isAuthenticated ? (
+        <Component {...props} />
+      ) : (
+        <Redirect
+          to={{
+            pathname: "/login",
+            state: { from: props.location }
+          }}
+        />
+      )
+    }
+  />
+);
+
+const Home = () => <h3>Home</h3>;
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <Navbar inverse>
-          <Navbar.Header>
-            <Navbar.Brand>
-              <a href="#home">UTN - Paquetes de graduación</a>
-            </Navbar.Brand>
-          </Navbar.Header>
-          <Nav>
-            <NavItem eventKey={1} href="#">
-              Paquetes de graduación
-            </NavItem>
-            <NavItem eventKey={2} href="#">
-              Formularios
-            </NavItem>
-            <NavItem eventKey={2} href="#">
-              Politicas de Abonos
-            </NavItem>
-            <NavItem eventKey={2} href="#">
-              Contactenos
-            </NavItem>
-          </Nav>
-        </Navbar>
-      </div>
+      <Router>
+        <div>
+          <Nabvar />
+          <div className="main-container">
+            <Switch>
+              <PrivateRoute path="/" exact component={Home} />
+              <Route path="/login" component={Login} />
+            </Switch>
+          </div>
+        </div>
+      </Router>
     );
   }
 }
